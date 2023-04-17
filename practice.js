225e72815951072a47925b61d61a2bf34abc383b
Amruth-111 <amruthkrishnaa@gmail.com>
2023-04-17 12:44:37 +0530

    my first rest Api project

--- practice.js
+++ practice.js
@@ -2,70 +2,110 @@ let form=document.getElementById("formid");
 let price=document.getElementById("price");
 let dish=document.getElementById("dish");
 let table=document.getElementById("table");
-
+let button=document.getElementById("button");
 let options=form.table.options
-let form2=document.getElementById("form2");
-
 let userlist1=document.getElementById("ul1");
 let userlist2=document.getElementById("ul2");
 let userlist3=document.getElementById("ul3");
 
 
-form.addEventListener("submit",onsubmit)
-form2.addEventListener("click",ondelete)
+//fetching data from browser
+axios.get('https://crudcrud.com/api/1e785583c9274374b55b0f570576b7f1/table')
+    .then((res)=>{
+        showbrowser(res.data)
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+
+function showbrowser(show){
+    let parentNode = userlist2;
+        
+        for (let i = 0; i < show.length; i++){
+            if(show[i].table==="Table 1"){
+                parentNode=userlist1
+           }else if(show[i].table==="Table 2"){
+                parentNode=userlist2
+           }else{ 
+                parentNode=userlist3
+           }
+            var childNode=`<li id=${show[i]._id} >${show[i].price}-${show[i].dish} - ${show[i].table}
+            <button onclick="deleteProduct('${show[i]._id}','${show[i].table}')">Delete</button>
+            </li>`
+            parentNode.innerHTML=parentNode.innerHTML+childNode;
+        }
+
+}     
 
 
+//deleting 
+function deleteProduct(key,show){
+    axios.delete(`https://crudcrud.com/api/1e785583c9274374b55b0f570576b7f1/table/${key}`)
+    .then(()=>{
+     removeScreen(key);
+    })
+    function removeScreen(key){
+      let parent=userlist1;
+      if(show==="Table 1"){
+        parent=userlist1
+        }else if(show==="Table 2"){
+            parent=userlist2
+        }else{ 
+            parent=userlist3
+        }
+      child=document.getElementById(key)
+      if(child){
+         parent.removeChild(child)
+      }
+   }
+   }   
 
-function onsubmit(e){
-e.preventDefault();
-let li=document.createElement("li");
-    li.setAttribute("id","newuser");
-    li.appendChild(document.createTextNode(`Price: ${price.value} Dish: ${dish.value} `));
+//submitting the data
+button.addEventListener("click",(e)=>{
 
-    let del=document.createElement('button');
-    del.appendChild(document.createTextNode('Delete'));
-    del.classList='delete';
-
-    li.appendChild(del);
-
-    if(this.table.value=="Table 1"){
-        userlist1.appendChild(li); 
-    }else if(this.table.value=="Table 2"){
-        userlist2.appendChild(li); 
-    }else{
-        userlist3.appendChild(li); 
-    }
+    e.preventDefault();
 
     let obj={
-    price: price.value,
-    dish: dish.value,
-    table:this.table.value
-    };
+        price: price.value,
+        dish: dish.value,
+        table:this.table.value
+        };
 
-    axios.post('https://crudcrud.com/api/135554898af54cdeab04f28259af50d7/table',obj)
-    .then((res)=>console.log(res))
-    .catch((err)=>console.log(err));
+    axios.post('https://crudcrud.com/api/1e785583c9274374b55b0f570576b7f1/table',obj)
+    .then((res1)=>{
+        console.log(res1);
+        showuseronScreen(res1.data)
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
 
-    axios.get('https://crudcrud.com/api/135554898af54cdeab04f28259af50d7/table')
-        .then((res)=>{
-            console.log(res)
-        })
-        .catch((err)=>{
-            console.error(err);
-        })
+    
+    
+    function showuseronScreen(show){
+        //console.log(show)
+        let parentNode =userlist2
+        //for (let i=show.length-1; i>show.length-2; i--){
+            if(show.table==="Table 1"){
+                parentNode=userlist1
+           }else if(show.table==="Table 2"){
+                parentNode=userlist2
+           }else{ 
+                parentNode=userlist3
+           }
+           var childNode=`<li id=${show._id} >${show.price}-${show.dish} - ${show.table}
+            <button onclick="deleteProduct('${show._id}','${show.table}')">Delete</button>
+            </li>`
+            parentNode.innerHTML=parentNode.innerHTML+childNode;
+    }
+//}
 
-        
-}
+    price.value="";
+    dish.value="";
 
-function ondelete(e){
-    e.preventDefault();
-    if(e.target.classList.contains("delete")){
-    let li=e.target.parentElement;
-    userlist1.removeChild(li);
-    userlist2.removeChild(li);
-    userlist1.removeChild(li);
-}
-}
+})
+
+
 
 
 
